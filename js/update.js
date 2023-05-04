502d3247e912ad2af3f7e32b807cc328b2d6d37a
kmafuna021 <kmafuna021@student.wethinkcode.co.za>
2023-05-05 00:28:59 +0200

    sit ready

---
+++ js/update.js
@@ -0,0 +1,22 @@
+var count = 0;
+
+function animProfessions(){
+  let proffessions = [
+    "Software_Development", 
+    "Mobile_Development", 
+    "Web_Development"
+  ]
+  var proffession = proffessions[count]
+  var animProfessionsEle = document.getElementById("animProfessions");
+
+  if(count<=proffessions.length-1){
+    console.log(count);
+    animProfessionsEle.innerHTML = proffession;
+    count++;
+  }else{
+    count = 0;
+  }
+}
+
+
+setInterval(animProfessions, 2000)
